c87f41fed93cdd1472f627d89de5e041c4ec4d20
OkanErturk16 <34457549+OkanErturk16@users.noreply.github.com>
2021-11-02 19:09:41 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,1 @@
-#%%
-import numpy as np
-from commpy.modulation import QAMModem, PSKModem
-from commpy.filters import rrcosfilter
-import matplotlib.pyplot as plt
-import time
-import scipy.signal as signal
-from scipy.interpolate import interp1d
 
